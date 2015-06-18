b157b9f890bac17fbe2b68c717d45789a8dc8bf6
Vadim Macagon <vadim.macagon@gmail.com>
2015-06-18 23:00:05 +0700

    Add test infrastructure and type definitions

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    'pkg': grunt.file.readJSON('package.json'),
+    'mochaTest': {
+      test: {
+        options: {
+          quiet: false,
+          clearRequireCache: true,
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    'tsd': {
+      test: {
+        options: {
+          command: 'reinstall',
+          latest: true,
+          config: 'conf/tsd-test.json',
+          opts: {
+            // props from tsd.Options
+          }
+        }
+      }
+    },
+    'tslint': {
+      errors: {
+        options: {
+          configuration: grunt.file.readJSON('conf/tslint.json')
+        },
+        files: {
+          src: [
+            'src/**/*.ts',
+            'test/**/*.ts'
+          ]
+        }
+      }
+    },
+    'typedoc': {
+      build: {
+        options: {
+          mode: 'file',
+          module: 'commonjs',
+          target: 'es5',
+          out: 'docs/',
+          name: '<%= pkg.name %>'
+        },
+        src: 'src/**/*.ts'
+      }
+    },
+    'typescript': {
+       lib: {
+         src: ['src/**/*.ts'],
+         dest: 'lib',
+         options: {
+           module: 'commonjs',
+           noImplicitAny: true,
+           sourceMap: true,
+           target: 'es5'
+         }
+       },
+       test: {
+         src: ['test/**/*.ts'],
+         options: {
+           module: 'commonjs',
+           sourceMap: true,
+           target: 'es5'
+         }
+       }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-tsd');
+  grunt.loadNpmTasks('grunt-tslint');
+  grunt.loadNpmTasks('grunt-typedoc');
+  grunt.loadNpmTasks('grunt-typescript');
+  
+  grunt.registerTask('docs', ['typedoc']);
+  
+  grunt.registerTask('build', ['typescript']);
+  
+  grunt.registerTask('run-tests', ['mochaTest:test']);
+  // FIXME: tslint can't handle decorators it seems so taking it out for now
+  grunt.registerTask('test', ['build', /*'tslint',*/ 'run-tests']);
+
+  grunt.registerTask('default', ['test']);
+};
---
+++ typings/test-tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="lib.es6.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="chai-as-promised/chai-as-promised.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="polymer/polymer.d.ts" />

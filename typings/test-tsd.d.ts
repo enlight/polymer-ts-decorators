fc2a7ff9f3c2da765776f808f7452294b4a14afd
Vadim Macagon <vadim.macagon@gmail.com>
2015-08-12 16:28:57 +0700

    Fix up references to .d.ts files again
    test-tsd.d.ts is generated by the tsd Grunt task so referencing typings
    obtained by other means in that file is probably not a good idea.

--- typings/test-tsd.d.ts
+++ typings/test-tsd.d.ts
@@ -1,6 +1,4 @@
-/// <reference path="lib.es6.d.ts" />
 /// <reference path="chai/chai.d.ts" />
 /// <reference path="chai-as-promised/chai-as-promised.d.ts" />
 /// <reference path="mocha/mocha.d.ts" />
 /// <reference path="node/node.d.ts" />
-/// <reference path="polymer/polymer.d.ts" />
